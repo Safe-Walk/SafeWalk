33737c1ed727c4f1127bc8b2bdb2d61f28e6c5d2
Bruno de Amorim Campos <amorim.software@gmail.com>
2018-10-10 23:07:14 -0300

    Inclusão da localidade do usuário no momento.

--- app/src/main/java/com/sw/safewalk/MapsActivity.java
+++ app/src/main/java/com/sw/safewalk/MapsActivity.java
@@ -2,9 +2,13 @@ package com.sw.safewalk;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,9 +16,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.*;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener{
+    private static final int FINE_LOCATION_PERMISSION_REQUEST = 1;
     private GoogleMap mMap;
 
     @Override
@@ -37,11 +42,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in my Location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//            mMap.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(sydney.latitude, sydney.longitude), 15));
+        System.out.println("Teste dessa porcaria!!!!");
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
+
+            System.out.println("Teste dessa degraça!!!!");
+        }else {
+            ActivityCompat.requestPermissions(this,
+                    new String []{Manifest.permission.ACCESS_FINE_LOCATION},
+                    FINE_LOCATION_PERMISSION_REQUEST);
         }
 
-//        mMap.setMyLocationEnabled(true);
     }
 
+    @Override
+    public void onMyLocationClick(@NonNull Location location){
+        Toast.makeText(this, "current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        return false;
+    }
 }
